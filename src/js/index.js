de7b0705579b652da652444d5d5c293f940e5ad0
Roman-Lahoda <lagoda9515@gmail.com>
2021-08-21 05:12:14 +0300

    some fix

--- src/js/index.js
+++ src/js/index.js
@@ -22,11 +22,27 @@ refs.clear.addEventListener("click", onClear);
 const showOneCounrty = obj => {
   const murkup = countryMurkup(obj);
   refs.result.innerHTML = murkup;
+
+  const population = document.querySelector(".population-js");
+  const refactPopulation = new Intl.NumberFormat("ru-RU").format(population.textContent);
+  population.textContent = refactPopulation;
+};
+
+const showMoreInfo = e => {
+  const targetCountry = e.target;
+  refs.input.value = targetCountry.textContent.trim();
+  refs.input.dispatchEvent(new Event("input"));
 };
 
 const showManyCountry = obj => {
+  console.log("test");
   const murkup = countriesMurkup(obj);
+  // console.log(murkup);
   refs.result.innerHTML = murkup;
+
+  const currentCountry = document.querySelector(".country-list");
+  console.log(currentCountry);
+  currentCountry.addEventListener("click", showMoreInfo);
 };
 
 const showAllert = () => {
@@ -50,7 +66,7 @@ const createMarkup = promiseResult => {
 };
 
 const errorfun = arrayCountry => {
-  console.log("errorfun");
+  refs.result.innerHTML = "";
   if (refs.input.value.trim().length >= 1 && arrayCountry.length === 0) {
     error({
       text: "Not found",
